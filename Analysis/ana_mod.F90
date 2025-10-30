b193be8ed9c61d647bc694e9c9ab1d2eff276395
Fakher Assaad <assaad@physik.uni-wuerzburg.de>
2025-10-30 11:53:50 +0100

    Fixed formatting issue

--- Analysis/ana_mod.F90
+++ Analysis/ana_mod.F90
@@ -1181,8 +1181,8 @@ Subroutine read_latt_hdf5(filename, name, sgn, bins, bins0, Latt, Latt_unit, dta
                   endif
                   CALL EXECUTE_COMMAND_LINE(command)
                   Open (Unit=10, File=File_out, status="unknown")
-                  Write(10, '(2(I11), E26.17E3, I11, A3)') &
-                       & Lt_eff, nbins/N_rebin, real(lt-1,kind(0.d0))*dtau, Latt_unit%Norb, Channel
+                  Write(10, '(2(I11), E26.17E3, I11," ", A3)') &
+                       & Lt_eff, nbins/N_rebin, real(lt-1,kind(0.d0))*dtau, Latt_unit%Norb, trim(Channel)
                   do nt = 1, LT_eff
                      Write(10, '(3(E26.17E3))') &
                           & dble(nt-1)*dtau,  dble(Xmean(nt)), sqrt(abs(dble(Xcov(nt,nt))))
